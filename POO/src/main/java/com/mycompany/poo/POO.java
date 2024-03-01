63d734110247e4bbb5c38a0c43054c328f5b0a77
KevinSpinell <kevinnallicarrascal@gmail.com>
2024-03-01 12:04:02 -0500

    Fin de clase 01/03

--- POO/src/main/java/com/mycompany/poo/POO.java
+++ POO/src/main/java/com/mycompany/poo/POO.java
@@ -1,58 +1,11 @@
 package com.mycompany.poo;
-
 public class POO {
     
     public static void main(String[] args) {
-        Character homero = new Character("Homero","Inspector de Seguridad", 38, "A la grande le puse cuca");
+        simpson homero = new simpson("Homero","Inspector de Seguridad", 38, "A la grande le puse cuca");
         homero.greeting();
-        Character bart = new Character("Bartolomeo", "Estudiante", 10, "Ay Caramba");
+        simpson bart = new simpson("Bartolomeo", "Estudiante", 10, "Ay Caramba");
         bart.greeting();
     }
-    static class Character{
-        String name;
-        String occupation;
-        int age;
-        String phrase;
-        
-        /*Constructor*/
-        public Character(String name, String occupation, int age, String phrase) {
-            this.name = name;
-            this.occupation = occupation;
-            this.age = age;
-            this.phrase = phrase;
-        }
-        public Character(){
-            
-        }
-        /*Getters and Setters*/
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public String getOccupation() {
-            return occupation;
-        }
-        public void setOccupation(String occupation) {
-            this.occupation = occupation;
-        }
-        public int getAge() {
-            return age;
-        }
-        public void setAge(int age) {
-            this.age = age;
-        }
-        public String getPhrase() {
-            return phrase;
-        }
-        public void setPhrase(String phrase) {
-            this.phrase = phrase;
-        }
-        void greeting(){
-            System.out.println(this.phrase);
-        }
-        
-        
-    }
+    
 }
