7ce4ec17da8c374129cb61e7e78af312e0f122cc
KevinSpinell <kevinnallicarrascal@gmail.com>
2024-03-15 09:08:26 -0500

    xd

---
+++ Acces_Levels/src/main/java/com/mycompany/acces_levels/Acces_Levels.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.acces_levels;
+
+/**
+ *
+ * @author SCIS2PC16
+ */
+public class Acces_Levels {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Acces_Levels/src/main/java/com/mycompany/acces_levels/OhterClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.acces_levels;
+
+/**
+ *
+ * @author SCIS2PC16
+ */
+public class OhterClass {
+    
+}
